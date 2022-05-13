d5485f36600ea7bb2b997a3f3b0f7407f8f2882d
Nitish Nirmal <nirmal567nitish@gmail.com>
2022-05-13 14:05:50 +0530

    Sum ofN added

---
+++ lecture0/SumOfN.java
@@ -0,0 +1,15 @@
+package lecture0;
+
+public class SumOfN {
+
+	public static void main(String[] args) {
+
+		System.out.println(sum(5));
+
+	}
+
+	public static int sum(int n) {
+		return (n) * (n + 1) / 2;
+	}
+
+}
