f15965a771a69b28e1cf50f187e31ebc198cb68c
Nitish Nirmal <nirmal567nitish@gmail.com>
2022-05-14 10:10:42 +0530

    message

---
+++ lec39/Stack_Using_Queue.java
@@ -0,0 +1,20 @@
+package lec39;
+
+public class Stack_Using_Queue {
+
+	public char[] peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void push(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public char[] pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
